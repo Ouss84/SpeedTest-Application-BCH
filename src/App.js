45b7afaf48fee4ec0f06fc7bb52fad2dc353e31e
Ouss84 <bahrioussama84@gmail.com>
2021-11-15 12:45:03 +0200

    adding Circle component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import "./App.css";
 import React, { Component } from "react";
 import Circle from "./Circle";
 class App extends Component {
+  state = {
+    circles: [
+      { id: 1, color: "light-bleu" },
+      { id: 2, color: "light-green" },
+      { id: 3, color: "light-red" },
+      { id: 4, color: "light-grey" },
+    ],
+  };
   render() {
     return (
       <div>
         <header> Speedy Gonzales</header>
         <div className="main">
           <h3 className="score-display">Your Score is: </h3>
-          <Circle />
+          <div className="circles">
+            {this.state.circles.map((c) => (
+              <Circle key={c.id} color={c.color} id={c.id} />
+            ))}
+          </div>
+
           <div className="button-wrapper">
             <button>Start</button>
             <button>Stop</button>
--- src/Circle.js
+++ src/Circle.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Circle = (props) => {
-  return <div className="circle">{props.id}</div>;
+  return <div className={`circle ${props.color}`}>{props.id} </div>;
 };
 
 export default Circle;
