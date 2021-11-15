f5f1ded31a4bc54fe074820f0b23b2f19501b058
Ouss84 <bahrioussama84@gmail.com>
2021-11-15 11:54:31 +0200

    Adding the general layout of the game page and styling it, initializing the Circle component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello world !</div>;
+import React, { Component } from "react";
+import Circle from "./Circle";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header> Speedy Gonzales</header>
+        <div className="main">
+          <h3 className="score-display">Your Score is: </h3>
+          <Circle />
+          <div className="button-wrapper">
+            <button>Start</button>
+            <button>Stop</button>
+          </div>
+        </div>
+        <footer>
+          Copyright-2021, Made by:<span>Ouss</span>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Circle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Circle = (props) => {
+  return <div className="circle">{props.id}</div>;
+};
+
+export default Circle;
