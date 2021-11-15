70d57ee4c878a2167ee092ea9b72c927fb900f5b
Ouss84 <bahrioussama84@gmail.com>
2021-11-15 13:00:29 +0200

    adding the clickHandler to increase the score

--- src/App.js
+++ src/App.js
@@ -10,22 +10,34 @@ class App extends Component {
       { id: 3, color: "lightsalmon" },
       { id: 4, color: "rgb(136, 151, 165)" },
     ],
+    score: 0,
+  };
+  clickHandler = () => {
+    this.setState({ score: this.state.score + 10 });
+  };
+  resetHandler = () => {
+    this.setState({ score: 0 });
   };
   render() {
     return (
       <div>
         <header> Speedy Gonzales</header>
         <div className="main">
-          <h3 className="score-display">Your Score is: </h3>
+          <h3 className="score-display">Your Score is: {this.state.score}</h3>
           <div className="circles">
             {this.state.circles.map((c) => (
-              <Circle key={c.id} color={c.color} id={c.id} />
+              <Circle
+                key={c.id}
+                color={c.color}
+                id={c.id}
+                click={this.clickHandler}
+              />
             ))}
           </div>
 
           <div className="button-wrapper">
             <button>Start</button>
-            <button>Stop</button>
+            <button onClick={this.resetHandler}>Stop</button>
           </div>
         </div>
         <footer>
